242806b3df67d9e80919c36a255e2fa0e0920c9b
DavidDevGt <josuedavidvl18@gmail.com>
2023-11-07 22:33:15 -0600

    Update title, add meta description and robots, and defer JS loading

--- templates/header.php
+++ templates/header.php
@@ -4,15 +4,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HojisVL Jewelry Shop</title>
+    <title>HojisVL Jewelry Shop | Joyas Únicas y Exclusivas</title>
+    <meta name="description" content="Descubre en HojisVL Jewelry Shop una selección exclusiva de joyas finas y accesorios. Encuentra el regalo perfecto o un capricho personal.">
+    <meta name="robots" content="index, follow">
     <!-- CSS -->
     <link href="assets/css/bootstrap.min.css" rel="stylesheet">
     <link href="assets/css/animate.min.css" rel="stylesheet">
     <link href="assets/css/custom.css" rel="stylesheet">
     <!-- JS -->
-    <script src="assets/js/jquery-3.7.1.min.js"></script>
-    <script src="assets/js/bootstrap.bundle.min.js"></script>
-    <script src="assets/js/custom.js"></script>
+    <script src="assets/js/jquery-3.7.1.min.js" defer></script>
+    <script src="assets/js/bootstrap.bundle.min.js" defer></script>
+    <script src="assets/js/custom.js" defer></script>
 </head>
 
 <body>

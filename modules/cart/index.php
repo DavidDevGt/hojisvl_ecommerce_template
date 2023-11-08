592c222b84883baf0afa2c3230e5f342dfc616b2
DavidDevGt <josuedavidvl18@gmail.com>
2023-11-07 22:31:22 -0600

    Add header and footer templates to admin, cart, and home modules

--- modules/cart/index.php
+++ modules/cart/index.php
@@ -1,1 +1,7 @@
-<?php
+<?php include_once '../../templates/header.php'; ?>
+
+<!-- Contenido específico del módulo de productos -->
+
+<script src="script.js"></script>
+
+<?php include_once '../../templates/footer.php'; ?>
